349aea399a279976e99a56da3fa6b0165597b462
Apurva Kumar <apurva7k@gmail.com>
2026-03-04 22:52:06 +0530

    Update API key configuration for Google AI

--- Projects/AI Agent/google ai.py
+++ Projects/AI Agent/google ai.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBSonnWlAMSWwrFGJ8pq1rjUI4j88iifnU")
+genai.configure(api_key="api")
 
 model = genai.GenerativeModel("gemini-2.5-flash")  # or whichever free model you want
 response = model.generate_content("Hello how are you")
